cd1bdf2dd5ef6cc0104da1837b1f1a4e9cd34928
Philip Woods <elzairthesorcerer@gmail.com>
2017-09-15 10:35:58 -0400

    Fixed toplevel documentation so it shows up in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
-/// This crate manages CPU affinities.
-/// 
-/// ## Example
-/// 
-/// This example shows how create a thread for each available processor and pin each thread to its corresponding processor. 
-/// 
-/// ```
-/// extern crate core_affinity;
-/// 
-/// use std::thread;
-/// 
-/// // Retrieve the IDs of all active CPU cores.
-/// let core_ids = core_affinity::get_core_ids().unwrap();
-/// 
-/// // Create a thread for each active CPU core.
-/// let handles = core_ids.into_iter().map(|id| {
-///     thread::spawn(move || {
-///         // Pin this thread to a single CPU core.
-///         core_affinity::set_for_current(id);
-///         // Do more work after this.
-///     })
-/// }).collect::<Vec<_>>();
-/// 
-/// for handle in handles.into_iter() {
-///     handle.join().unwrap();
-/// }
-/// ```
+//! This crate manages CPU affinities.
+//! 
+//! ## Example
+//! 
+//! This example shows how create a thread for each available processor and pin each thread to its corresponding processor. 
+//! 
+//! ```
+//! extern crate core_affinity;
+//! 
+//! use std::thread;
+//! 
+//! // Retrieve the IDs of all active CPU cores.
+//! let core_ids = core_affinity::get_core_ids().unwrap();
+//! 
+//! // Create a thread for each active CPU core.
+//! let handles = core_ids.into_iter().map(|id| {
+//!     thread::spawn(move || {
+//!         // Pin this thread to a single CPU core.
+//!         core_affinity::set_for_current(id);
+//!         // Do more work after this.
+//!     })
+//! }).collect::<Vec<_>>();
+//! 
+//! for handle in handles.into_iter() {
+//!     handle.join().unwrap();
+//! }
+//! ```
 
 #[cfg(test)]
 extern crate num_cpus;
