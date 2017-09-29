9e537cc95e7e9b33eb510222d6322606da3aa171
Philip Woods <elzairthesorcerer@gmail.com>
2017-09-29 17:07:53 -0400

    Hopefully fixed the OSX build error

--- src/lib.rs
+++ src/lib.rs
@@ -336,7 +336,6 @@ mod macos {
     type thread_policy_t = *mut thread_affinity_policy_data_t;
 
     const THREAD_AFFINITY_POLICY: thread_policy_flavor_t = 4;
-    static THREAD_AFFINITY_POLICY_COUNT: mach_msg_type_number_t = mem::size_of::<thread_affinity_policy_data_t>() as mach_msg_type_number_t / mem::size_of::<integer_t>() as mach_msg_type_number_t;
 
     #[link(name = "System", kind = "framework")]
     extern {
@@ -355,6 +354,8 @@ mod macos {
     }
 
     pub fn set_for_current(core_id: CoreId) {
+        let THREAD_AFFINITY_POLICY_COUNT: mach_msg_type_number_t = mem::size_of::<thread_affinity_policy_data_t>() as mach_msg_type_number_t / mem::size_of::<integer_t>() as mach_msg_type_number_t;
+
         let mut info = thread_affinity_policy_data_t {
             affinity_tag: core_id.id as integer_t,
         };
